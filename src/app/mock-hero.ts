b822340b8434ab7975447e73a7e2a343f0fc378b
Amol Dumbre <amol.dumbre@hp.com>
2018-04-12 12:38:27 +0530

    Initial commit : Array edit & update

---
+++ src/app/mock-hero.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+	{id:1,name:"Amol"},
+	{id:2,name:"Amit"},
+	{id:3,name:"Ashish"},
+	{id:4,name:"Ankit"},
+	{id:5,name:"Alok"},
+	{id:6,name:"Anvit"},
+	{id:7,name:"Akansha"},
+	{id:8,name:"Akruti"},
+	{id:9,name:"Akashara"},
+	{id:10,name:"Anusaya"},		
+]
